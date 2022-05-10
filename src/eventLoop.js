14d17013e6d67ec270e70ea5386a53be70dd2b59
Julien <jujufrancois1999@gmail.com>
2022-05-10 14:49:38 +0200

    Event Loop test

---
+++ src/eventLoop.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const FILEPATH = path.join(__dirname, '..', 'media', 'grunter.JPG');
+
+// console.time("load sync");
+// const buffer = fs.readFileSync(FILEPATH);
+// console.timeEnd("load sync");
+
+// console.log(buffer);   
+
+const imgReady = (err, buf) => {
+    console.log(buf)
+}
+
+console.time("load sync");
+const buffer2 = fs.readFile(FILEPATH, imgReady);
+console.timeEnd("load sync");
+
+console.log("end.");
